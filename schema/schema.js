706e6aa7bdac16d93c37c1ea08e1226daf41090c
Yuriy Rypka <yurarypka@gmail.com>
2018-01-10 23:33:11 +0200

    add graphql root query and resolve with hardcoded users data

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema,
 } = graphql;
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+];
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -15,15 +22,20 @@ const UserType = new GraphQLObjectType({
   }
 });
 
-const RootQueryType = new GraphQLObjectType({
-  name: 'RootQuery',
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
   fields: {
     user: {
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
-
+        return _.find(users, { id: args.id });
+        // return users.find(user => user.id === args.id);
       }
     }
   }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
