c4ed446fa32401471d5abba2e17eb414eb13a367
Yuriy Rypka <yurarypka@gmail.com>
2018-01-11 17:40:20 +0200

    add editUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -90,6 +90,20 @@ const mutation = new GraphQLObjectType({
         return axios.delete(`http://localhost:3000/users/${ id }`)
           .then(resp => resp.data);
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        const { id, ...rest } = args;
+        return axios.patch(`http://localhost:3000/users/${ id }`, { ...rest })
+          .then(resp => resp.data);
+      }
     }
   }
 });
