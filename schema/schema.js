0ef5a950dcf73bb8546a024139a04b7423e1ae58
Yuriy Rypka <yurarypka@gmail.com>
2018-01-11 05:44:18 +0200

    add users field to company graphql schema
    Bidirectional Relations
    Resolving Circular References

--- schema/schema.js
+++ schema/schema.js
@@ -6,21 +6,30 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql;
 
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  }
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(resp => resp.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -31,7 +40,7 @@ const UserType = new GraphQLObjectType({
           .then(resp => resp.data);
       }
     },
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
